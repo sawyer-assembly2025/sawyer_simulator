642b6f65f9c56f2e1bb5a5f485f6fcc3f9ec973d
sawyer-assembly2025 <hquijadaval@gmail.com>
2025-11-26 05:43:21 +0000

    routine added in examples to adquire data while moving the peg in the hole to adquire primitive measurements

--- sawyer_sim_examples/scripts/obtain_ftdata_at_home.py
+++ sawyer_sim_examples/scripts/obtain_ftdata_at_home.py
@@ -22,30 +22,38 @@ from geometry_msgs.msg import WrenchStamped
 
 #Global variables for force and torque
 #Need to be global to be updated by call back function in subscriber
-ati_ft_data = np.zeros((1,6))
-ft_samples = np.zeros((1,6))
+ati_ft_data_ext = np.zeros((1,12))
+ft_samples = np.zeros((1,12))
 
-def obtain_ftdata(data):
+def obtain_ftdata(data,args):
 	
     global ft_samples
 
     #Split wrench data
-    ati_ft_data[0][0] = data.wrench.force.x
-    ati_ft_data[0][1] = data.wrench.force.y
-    ati_ft_data[0][2] = data.wrench.force.z
-    ati_ft_data[0][3] = data.wrench.torque.x
-    ati_ft_data[0][4] = data.wrench.torque.y
-    ati_ft_data[0][5] = data.wrench.torque.z
+    ati_ft_data_ext[0][0] = data.wrench.force.x
+    ati_ft_data_ext[0][1] = data.wrench.force.y
+    ati_ft_data_ext[0][2] = data.wrench.force.z
+    ati_ft_data_ext[0][3] = data.wrench.torque.x
+    ati_ft_data_ext[0][4] = data.wrench.torque.y
+    ati_ft_data_ext[0][5] = data.wrench.torque.z
+    ati_ft_data_ext[0][6] = args[0]
+    ati_ft_data_ext[0][7] = args[1]
+    ati_ft_data_ext[0][8] = args[2]
+    ati_ft_data_ext[0][9] = args[3]
+    ati_ft_data_ext[0][10] = args[4]
+    ati_ft_data_ext[0][11] = args[5]
 
     #Stack measurement at call back
-    ft_samples = np.vstack((ft_samples,ati_ft_data))
+    ft_samples = np.vstack((ft_samples,ati_ft_data_ext))
     
-def adquisition_cycle(test_time=1.0):
+def adquisition_cycle(test_time=1.0,direction="",category=[]):
 
     global ft_samples
-
+    ft_samples = np.zeros((1,12))
+    arg = category
+    
     try:
-        rospy.Subscriber("/robot/ft_sensor_topic/", WrenchStamped, obtain_ftdata)
+        sub = rospy.Subscriber("/robot/ft_sensor_topic/", WrenchStamped, obtain_ftdata, callback_args=(arg[0][0],arg[0][1],arg[0][2],arg[0][3],arg[0][4],arg[0][5]))
 
         #Define the duration you want the subscriber to run (e.g., 10 seconds)
         duration = rospy.Duration(test_time)
@@ -66,7 +74,7 @@ def adquisition_cycle(test_time=1.0):
 
         #Save all data to a csv file
         current_datetime = datetime.now().strftime("%d%m%Y_%H_%M_%S")
-        ATI_net_path = "samples_" + current_datetime + ".csv"
+        ATI_net_path = "samples_" + current_datetime + "_" + direction + ".csv"
         rospack = rospkg.RosPack()
         package_path = rospack.get_path('sawyer_sim_examples')
         csv_file_path = os.path.join(package_path, ATI_net_path)
@@ -74,17 +82,38 @@ def adquisition_cycle(test_time=1.0):
 
         rospy.loginfo("Data saved succesfully")
 
-        # Explicitly shutdown the node after the duration
-        rospy.signal_shutdown("Finishing testing node")
+        #Unsubscribe to topic to keep node running
+        sub.unregister()
     
     except rospy.ROSInterruptException:
 
         # Explicitly shutdown the node after the duration
         rospy.loginfo("Test unsuccesful, finishing testing node")
+        rospy.signal_shutdown("Finishing testing node")
 
 
 if __name__ == '__main__':
 
+    test_movements = [
+                    ("None", np.array([[1., 0., 0., 0., 0., 0.]])), 
+                    ("X+", np.array([[1., 0., 0., 0., 0., 1.]])), 
+                    ("X-", np.array([[1., 0., 0., 0., 1., 0.]])),
+                    ("Y+", np.array([[1., 0., 0., 0., 1., 1.]])),
+                    ("Y-", np.array([[1., 0., 0., 1., 0., 0.]])),
+                    ("Z+", np.array([[1., 0., 0., 1., 0., 1.]])),
+                    ("Z-", np.array([[1., 0., 0., 1., 1., 0.]])),
+                    ("RotX+", np.array([[1., 0., 0., 1., 1., 1.]])),
+                    ("RotX-", np.array([[1., 0., 1., 0., 0., 0.]])),
+                    ("RotY+", np.array([[1., 0., 1., 0., 0., 1.]])),
+                    ("RotY-", np.array([[1., 0., 1., 0., 1., 0.]])),
+                    ("RotZ+", np.array([[1., 0., 1., 0., 1., 1.]])),
+                    ("RotZ-", np.array([[1., 0., 1., 1., 0., 0.]])),
+                    ("X+Y+", np.array([[1., 0., 1., 1., 0., 1.]])),
+                    ("X+Y-", np.array([[1., 0., 1., 1., 1., 0.]])),
+                    ("X-Y+", np.array([[1., 0., 1., 1., 1., 1.]])),
+                    ("X-Y-", np.array([[1., 1., 0., 0., 0., 0.]]))
+                    ]
+    
     try:
         #Initialize Example node
         rospy.init_node('sawyer_ftdata_home', anonymous=False)
@@ -95,14 +124,31 @@ if __name__ == '__main__':
         #Wait 2 seconds
         time.sleep(2)
 
+        if sawyer._is_clicksmart == True:
+            sawyer.set_red_light()
+        
         #Move robot to home
         sawyer.move_to_home()
 
         #Wait 2 seconds
         time.sleep(2)
 
-        #Start adquiring data
-        adquisition_cycle(test_time=1.0)
+        for movement in test_movements:
+            
+            if sawyer._is_clicksmart == True:
+                sawyer.set_green_light()
+            
+            #Start adquiring data
+            adquisition_cycle(test_time=5.0,direction=movement[0],category=movement[1])
+            
+            if sawyer._is_clicksmart == True:
+                sawyer.set_blue_light()
+            
+            #Wait 2 seconds
+            time.sleep(2)
+            
+        # Explicitly shutdown the node after the duration
+        rospy.signal_shutdown("Finishing testing node")
     
     except rospy.ROSInterruptException:
         rospy.loginfo("Test unsuccesful, finishing testing node")
